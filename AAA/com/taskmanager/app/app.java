cfcbbe9cd4222484adfbde7c723969c6b3ab81ef
Kekonator31 <48918994+Kekonator31@users.noreply.github.com>
2025-03-13 00:06:12 +0200

    Final

--- AAA/com/taskmanager/app/app.java
+++ AAA/com/taskmanager/app/app.java
@@ -1,6 +1,6 @@
 package com.taskmanager.app;
 import com.taskmanager.services.*;
-
+import java.time.LocalDateTime;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -9,7 +9,7 @@ public class app {
 
         TaskService taskService = new TaskService();
 
-        taskService.addTask("Go to technic", "Wait for bus and go to school", new Date(125 + 1900, Calendar.MARCH, 11, 6, 0, 0));
+        taskService.addTask("Go to technic", "Wait for bus and go to school", new Date(125, Calendar.MARCH, 11, 6, 0, 0));
 
         taskService.addTask("Learn in technic", "Learn many thinks in technic");
 
